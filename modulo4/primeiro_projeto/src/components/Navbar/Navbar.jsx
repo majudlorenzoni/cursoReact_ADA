7581f3ffbafe0ee555d42283367a30c69975d3d0
majudlorenzoni <juliamaria892@gmail.com>
2024-04-25 21:02:39 -0300

    React Legado I - Componentização

---
+++ modulo4/primeiro_projeto/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logoImg from '../../assets/images/img1.png';
+import "./styles.css"
+
+class Navbar extends React.Component {
+  render(){
+    return (
+      <header>
+      <nav id="navbar">
+        <div className="nav-brand">
+          <img src={logoImg} alt="" />
+          <h1>Space Flight News</h1>
+        </div>
+
+        <ul className="nav-list">
+        <li><a href="/">Home</a></li>
+        <li><a href="/">Trendign</a></li>
+        <li><a href="/">Categories</a></li>
+        <li><a href="/">Abouts us</a></li>
+        <li><a href="/">Home</a></li>
+        </ul>
+      </nav>
+    </header>
+    )
+  }
+}
+
+export default Navbar;
